77629099b1661f9b5462e2dbb72ecb4f9100d5d0
Dilmurad7 <deel.nab93@gmail.com>
2021-11-11 23:05:24 +0400

    final bulid

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
+import logo from './ozero_gory_derevia_129959_1920x1080.jpg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reac
-        </a>
+
+        <img src={logo} className="App-logo" alt="logo"/>
+        <p className="par">Это озеро</p>
+        
       </header>
     </div>
   );
